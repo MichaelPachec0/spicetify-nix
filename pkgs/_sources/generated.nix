96a7cb1b6b13d44b4c23fb4228cc28206c22d09f
the-argus <the-argus@users.noreply.github.com>
2023-08-23 18:45:12 +0000

    Bump Spicetify Packages

--- pkgs/_sources/generated.nix
+++ pkgs/_sources/generated.nix
@@ -42,16 +42,16 @@
   };
   charlieS1103Src = {
     pname = "charlieS1103Src";
-    version = "c3e8cdbc5060a42be6791e2da7a797f409f9bb73";
+    version = "ee39fbc301143bdbd92fad948835c1e46ddc0b9c";
     src = fetchgit {
       url = "https://github.com/CharlieS1103/spicetify-extensions";
-      rev = "c3e8cdbc5060a42be6791e2da7a797f409f9bb73";
+      rev = "ee39fbc301143bdbd92fad948835c1e46ddc0b9c";
       fetchSubmodules = false;
       deepClone = false;
       leaveDotGit = false;
-      sha256 = "sha256-qS0ufikbLVCSyXTNh30n+QdmR2fQbY3/tg9UYSN0RfA=";
+      sha256 = "sha256-OgSK05PEu6bUsFippPIpNJoWtLjCw4n/tmlVsAOJBRA=";
     };
-    date = "2023-08-18";
+    date = "2023-08-23";
   };
   comfySrc = {
     pname = "comfySrc";
