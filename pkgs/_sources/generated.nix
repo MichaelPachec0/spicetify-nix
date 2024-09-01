1577d78a5865f918f460660ce9e8c68975e06826
MichaelPachec0 <MichaelPachec0@users.noreply.github.com>
2024-09-01 18:44:50 +0000

    chore: Bump Spicetify Packages

--- pkgs/_sources/generated.nix
+++ pkgs/_sources/generated.nix
@@ -241,31 +241,31 @@
   };
   marketplaceSrc = {
     pname = "marketplaceSrc";
-    version = "49b7dfd515baa85a68dac4fc9825ee7e2016472d";
+    version = "3039016c53bdd85042c8901c82c817c6c524b243";
     src = fetchgit {
       url = "https://github.com/spicetify/spicetify-marketplace";
-      rev = "49b7dfd515baa85a68dac4fc9825ee7e2016472d";
+      rev = "3039016c53bdd85042c8901c82c817c6c524b243";
       fetchSubmodules = false;
       deepClone = false;
       leaveDotGit = false;
       sparseCheckout = [ ];
-      sha256 = "sha256-uIUIu9oJKLPUWiK26xs3XMreU874xE7D5kdy7eAFW1Q=";
+      sha256 = "sha256-T/Zrqn8im/3M7heOiMOrK6FmSsJnR4RglCZOeEiHgWg=";
     };
-    date = "2024-08-27";
+    date = "2024-09-01";
   };
   nameThatTuneSrc = {
     pname = "nameThatTuneSrc";
-    version = "bbe78ab2c65eda8950b6da26c7065b0238fe20d3";
+    version = "7641c98264556dc50698a1d7754070ae90569aad";
     src = fetchgit {
       url = "https://github.com/theRealPadster/name-that-tune";
-      rev = "bbe78ab2c65eda8950b6da26c7065b0238fe20d3";
+      rev = "7641c98264556dc50698a1d7754070ae90569aad";
       fetchSubmodules = false;
       deepClone = false;
       leaveDotGit = false;
       sparseCheckout = [ ];
-      sha256 = "sha256-3zBopcQ8AbQu6GDEePVDjBf/f7H9YcJrzWkD8r2qyac=";
+      sha256 = "sha256-oyn1AWk6ncg8V5y7zHHX8gy4SSOtKoSTan1FqoIpZYI=";
     };
-    date = "2024-07-24";
+    date = "2024-09-01";
   };
   nordSrc = {
     pname = "nordSrc";
